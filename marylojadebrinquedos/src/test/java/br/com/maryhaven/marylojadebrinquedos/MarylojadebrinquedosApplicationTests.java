4275dab63ffc2a85a92380566875427fb7e6f193
LaraALVV <85951647+LaraALVV@users.noreply.github.com>
2023-11-30 20:27:46 -0300

    Primeiras onfigurações

---
+++ marylojadebrinquedos/src/test/java/br/com/maryhaven/marylojadebrinquedos/MarylojadebrinquedosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maryhaven.marylojadebrinquedos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarylojadebrinquedosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
